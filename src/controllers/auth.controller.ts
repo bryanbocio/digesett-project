afd9d3cbdac7f000f918f9fac3bd79cc5ced7420
bryanbocio <alexanderbocio@gmail.com>
2022-09-25 19:02:46 -0400

    version beta de el login de usuarios API

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -30,6 +30,8 @@ export const signin=async (req:Request, res:Response)=>{
     });
     res.header('auth-token',token).json(user);
 }
-export const profile=(req:Request, res:Response)=>{
-    res.send('profile')
+export const profile=async (req:Request, res:Response)=>{
+  const user= await User.findById(req.userId, {password:0});
+  if(!user) return res.status(404).json('user not found');
+  res.json(user);
 }
