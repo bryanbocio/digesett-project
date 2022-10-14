1e915337968261fd08441ac0796a7616d3feb737
bryanbocio <alexanderbocio@gmail.com>
2022-10-13 23:02:42 -0400

    agregando front end de como se van a visualizar las multas, primera version, estará sometida a cambios

--- client-digesett/src/app/app-routing.module.ts
+++ client-digesett/src/app/app-routing.module.ts
@@ -16,6 +16,10 @@ const routes: Routes = [
     path: 'signup',
     loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
   },
+  {
+    path: 'principal',
+    loadChildren: () => import('./principal/principal.module').then( m => m.PrincipalPageModule)
+  },
 ];
 
 @NgModule({
